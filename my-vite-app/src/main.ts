a56fb3dd09fbcd2dee253401ada553cced00f4c6
AZazwinner <victorjiangvj@gmail.com>
2025-07-13 10:20:07 -0700

    update

--- my-vite-app/src/main.ts
+++ my-vite-app/src/main.ts
@@ -22,3 +22,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+fetch("https://1393998996585844816.discordsays.com/.proxy/api/user/849827666064048178", {
+  method: "GET"
+})
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error("Fetch error:", error));
